3d456f62577d65743d2aca0a1477ace3c1587061
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2023-05-04 20:31:48 +1000

    Created some functions to split option1() in functions.hpp. It was overly large. Will add comments later.

--- include/functions.hpp
+++ include/functions.hpp
@@ -6,20 +6,23 @@
 #define PROJECT_CODE_FUNCTIONS_HPP
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 #include "utilities.hpp"
 
 void prompt();
 
 // option 1 functions
 void option1();
-bool check_id_exists(std::ifstream& file, const std::string& id);
-Symptoms pick_symptoms(std::ifstream& file);
-std::vector<std::string> pick_high_risk_visited_locations(std::ifstream& file);
 
+bool check_id_exists(std::ifstream& file, const std::string& id);
 void get_id(std::ifstream& patient_details, PatientRecord& record);
 void get_name(PatientRecord& record);
 void get_dob(PatientRecord& record);
 void get_address(PatientRecord& record);
 void get_overseas_travel(PatientRecord& record);
+
+void pick_symptoms(PatientRecord& record, std::ifstream& file);
+
+void pick_high_risk_visited_locations(PatientRecord& record, std::ifstream& file);
 
 #endif //PROJECT_CODE_FUNCTIONS_HPP
--- src/functions.cpp
+++ src/functions.cpp
@@ -21,10 +21,11 @@ void option1()
     std::ifstream symptoms_file("../symptoms.txt");
     std::ifstream high_risk_loc_file("../high_risk_covid_locations.txt");
 
+    // checking if all files are opened
     if (!patient_details.is_open() || !symptoms_file.is_open() || !high_risk_loc_file.is_open())
         throw std::runtime_error("Option1() - Failed to open all files");
 
-    {
+    { // checking is symptoms database is populated
         std::string str;
         std::getline(symptoms_file, str); // skip header
         std::getline(symptoms_file, str);
@@ -43,14 +44,32 @@ void option1()
     get_dob(record);
     get_address(record);
     get_overseas_travel(record);
-    record.symptoms = std::move(pick_symptoms(symptoms_file));
-    record.visited_high_risk_locations = std::move(pick_high_risk_visited_locations(high_risk_loc_file));
+    pick_symptoms(record, symptoms_file);
+    pick_high_risk_visited_locations(record, high_risk_loc_file);
 
     patient_details.close();
     symptoms_file.close();
     high_risk_loc_file.close();
 
     // implement algorithm for recommending test
+}
+
+bool check_id_exists(std::ifstream& file, const std::string& id)
+{
+    std::string line;
+    std::getline(file, line); // discard header
+
+    while (std::getline(file, line))
+    {
+        std::stringstream ss(line);
+        std::string existing_id;
+        std::getline(ss, existing_id, ',');
+
+        if (id == existing_id)
+            return true;
+    }
+
+    return false;
 }
 
 void get_id(std::ifstream& patient_details, PatientRecord& record)
@@ -68,10 +87,13 @@ void get_id(std::ifstream& patient_details, PatientRecord& record)
 
 void get_name(PatientRecord& record)
 {
+    // this function gets the fullname of the patient
+    // the input must not be empty or contain a comma, since that can mess up searching later
+
     std::cout << "Enter first name:";
     std::getline(std::cin, record.fname);
 
-    while (record.fname == "")
+    while (record.fname == "" || record.fname.contains(','))
     {
         std::cout << "INVALID NAME\n";
         std::cout << "Enter first name:";
@@ -81,7 +103,7 @@ void get_name(PatientRecord& record)
     std::cout << "Enter last name:";
     std::getline(std::cin, record.lname);
 
-    while (record.lname == "")
+    while (record.lname == "" || record.lname.contains(','))
     {
         std::cout << "INVALID NAME\n";
         std::cout << "Enter last name:";
@@ -107,7 +129,7 @@ void get_address(PatientRecord& record)
     std::cout << "Enter address:";
     std::getline(std::cin, record.address);
 
-    while (record.address == "")
+    while (record.address == "" || record.address.contains(','))
     {
         std::cout << "INVALID ADDRESS\n";
         std::cout << "Enter address:";
@@ -128,7 +150,7 @@ void get_overseas_travel(PatientRecord& record)
     }
 }
 
-Symptoms pick_symptoms(std::ifstream& file)
+void pick_symptoms(PatientRecord& record, std::ifstream& file)
 {
     std::string line;
     std::getline(file, line); // discard header
@@ -154,19 +176,20 @@ Symptoms pick_symptoms(std::ifstream& file)
 
     std::getline(std::cin, line);
 
-    if (line == "") return Symptoms();
+    if (line == "") return; // patient has no symptoms
 
     std::stringstream ss(line);
     std::string num;
     std::vector<int> selected_symptoms;
 
+    // validating input
     while (std::getline(ss, num, ','))
     {
         try
         {
             int symptom_id = std::stoi(num);
 
-            if (symptom_id > symptoms_map.size()) continue;
+            if (symptom_id > symptoms_map.size()) continue; // checks if num entered is larger than the list
 
             selected_symptoms.push_back(symptom_id);
         }
@@ -176,26 +199,31 @@ Symptoms pick_symptoms(std::ifstream& file)
         }
     }
 
-    Symptoms symptoms;
+    // checks if the symptom is already in the list. If it isn't, then the symptom will be added
+    auto add_symptom = [] (std::vector<std::string>& vec, const std::string& str) {
+        if (std::find(vec.begin(), vec.end(), str) == vec.end())
+            vec.push_back(str);
+    };
+
+    Symptoms& symptoms = record.symptoms;
+
     for (int symptom_id : selected_symptoms)
     {
         auto& symptom = symptoms_map[symptom_id];
 
         // check if symptom is already in vector
         if (symptom.second == "high")
-            symptoms.high_risk_symptoms.push_back(symptom.first);
+            add_symptom(symptoms.high_risk_symptoms, symptom.first);
         else if (symptom.second == "medium")
-            symptoms.medium_risk_symptoms.push_back(symptom.first);
+            add_symptom(symptoms.medium_risk_symptoms, symptom.first);
         else if (symptom.second == "low")
-            symptoms.low_risk_symptoms.push_back(symptom.first);
+            add_symptom(symptoms.low_risk_symptoms, symptom.first);
     }
-
-    return symptoms;
 }
 
-std::vector<std::string> pick_high_risk_visited_locations(std::ifstream& file)
+void pick_high_risk_visited_locations(PatientRecord& record, std::ifstream& file)
 {
-    {
+    { // checking if database is empty
         std::string str;
         std::getline(file, str);
         file.seekg(0);
@@ -203,18 +231,18 @@ std::vector<std::string> pick_high_risk_visited_locations(std::ifstream& file)
         if (str == "")
         {
             std::cout << "NO HIGH RISK LOCATIONS IN DATABASE";;
-            return std::vector<std::string>();
+            return; // there are no high risks locations in database so this part is skipped
         }
     }
 
     std::unordered_map<int, std::string> hr_locations_map;
     std::string line;
-
     int id = 1;
+
+    // printing all locations and adding them to a map
     while (std::getline(file, line))
     {
         hr_locations_map[id] = line;
-
         std::transform(line.begin(), line.end(), line.begin(), ::toupper);
         std::cout << id << " - " << line << '\n';
 
@@ -226,19 +254,20 @@ std::vector<std::string> pick_high_risk_visited_locations(std::ifstream& file)
 
     std::getline(std::cin, line);
 
-    if (line == "") return std::vector<std::string>();
+    if (line == "") return; // patient hasn't been to any high risk location
 
     std::stringstream ss(line);
     std::string num;
     std::vector<int> selected_locations;
 
+    // validating input
     while (std::getline(ss, num, ','))
     {
         try
         {
             int location_id = std::stoi(num);
 
-            if (location_id > hr_locations_map.size()) continue;
+            if (location_id > hr_locations_map.size()) continue;  // checks if num entered is larger than the list
 
             selected_locations.push_back(location_id);
         }
@@ -248,31 +277,13 @@ std::vector<std::string> pick_high_risk_visited_locations(std::ifstream& file)
         }
     }
 
-    std::vector<std::string> visited_locations;
+    std::vector<std::string>& visited_locations = record.visited_high_risk_locations;
     for (int location_id : selected_locations)
     {
-        // check if visited location is already in vector
         std::string& visited_location = hr_locations_map[location_id];
-        visited_locations.push_back(visited_location);
+
+        // checks if location is already in the list before adding
+        if (std::find(visited_locations.begin(), visited_locations.end(), visited_location) == visited_locations.end())
+            visited_locations.push_back(visited_location);
     }
-
-    return visited_locations;
-}
-
-bool check_id_exists(std::ifstream& file, const std::string& id)
-{
-    std::string line;
-    std::getline(file, line); // discard header
-
-    while (std::getline(file, line))
-    {
-        std::stringstream ss(line);
-        std::string existing_id;
-        std::getline(ss, existing_id, ',');
-
-        if (id == existing_id)
-            return true;
-    }
-
-    return false;
 }
